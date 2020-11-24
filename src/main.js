94b125fc7ba87b83ff4b7ed84cc3a00a5a21cc38
Sasha <ukemi-atemi@ya.ru>
2020-11-24 23:59:40 +0300

    В main.js удалены излишние вложенности итераторов

--- src/main.js
+++ src/main.js
@@ -18,7 +18,6 @@ const render = (container, template, place) => {
 const siteHeaderElement = document.querySelector(`.header`);
 // Звание пользователя
 render(siteHeaderElement, createUserNameTemplate(), `beforeend`);
-
 const siteMainElement = document.querySelector(`.main`);
 // Меню
 render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
@@ -26,30 +25,32 @@ render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
 render(siteMainElement, createFilterTemplate(), `beforeend`);
 // Контент
 render(siteMainElement, createMainContentTemplate(), `beforeend`);
-
-const siteMoviesElements = Array.from(siteMainElement.querySelectorAll(`.films-list`));
-siteMoviesElements.forEach(item => {
-  const siteMoviesContainerElements = Array.from(item.querySelectorAll(`.films-list__container`));
-  if (!item.classList.contains('films-list--extra')) {
-    siteMoviesContainerElements.forEach(item => {
-      for (let i = 0; i < MOVIE_COUNT; i++) {
-        // Карточки фильмов для главного списка
-        render(item, createMovieTemplate(), `beforeend`);
-      }
-    });
-    // Кнопка "Показать больше"
-    render(item, createShowMoreButtonTemplate(), `beforeend`);
-  }
-  else {
-      siteMoviesContainerElements.forEach(item => {
-        for (let i = 0; i < MOVIE_EXTRA_COUNT; i++) {
-          // Карточки фильмов для списков "Top rated" и "Most commented"
-          render(item, createMovieTemplate(), `beforeend`);
-        }
-    });
-  }
-});
-
+// Контейнер с заголовком и списком фильмов
+const siteMoviesBoxElement = siteMainElement.querySelector(`.films-list`);
+// Список фильмов
+const siteMoviesListElement = siteMainElement.querySelector(`.films-list__container`);
+for (let i = 0; i < MOVIE_COUNT; i++) {
+  // Карточки фильмов для главного списка
+  render(siteMoviesListElement, createMovieTemplate(), `beforeend`);
+}
+// Кнопка "Показать больше"
+render(siteMoviesBoxElement, createShowMoreButtonTemplate(), `beforeend`);
+// Контейнер с заголовком и списком "Top rated"
+const siteTopRatedMoviesBoxElement = siteMainElement.querySelector(`.films-list.films-list--extra`);
+// Список фильмов "Top rated"
+const siteTopRatedMoviesListElement = siteTopRatedMoviesBoxElement.querySelector(`.films-list__container`);
+for (let i = 0; i < MOVIE_EXTRA_COUNT; i++) {
+  // Карточки фильмов для списка "Top rated"
+  render(siteTopRatedMoviesListElement, createMovieTemplate(), `beforeend`);
+}
+// Контейнер с заголовком и списком "Most commented"
+const siteMostCommentedMoviesBoxElement = siteMainElement.querySelectorAll(`.films-list.films-list--extra`)[1];
+// Список фильмов "Most commented"
+const siteMostCommentedMoviesListElement = siteMostCommentedMoviesBoxElement.querySelector(`.films-list__container`);
+for (let i = 0; i < MOVIE_EXTRA_COUNT; i++) {
+  // Карточки фильмов для списка "Most commented"
+  render(siteMostCommentedMoviesListElement, createMovieTemplate(), `beforeend`);
+}
 const siteFooterElement = document.querySelector(`.footer`);
 const siteStatisticsFooterElement = siteFooterElement.querySelector(`.footer__statistics`);
 // Статистика в footer
